adf9a027f775e0b8f76c456c68901bf51f3da2be
lerttosprorn <lerttosporn.s@ku.th>
2021-03-11 17:44:17 +0700

    ดาว

---
+++ 6320503084_5_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,x,z,a,u;
+    scanf("%d",&n);
+        if(n%2==0)
+    {
+        z=n/2;
+        a=n-1;
+        u=z;
+    }
+    else
+    {
+
+    z=(n+1)/2;
+    a=n;
+    u=z-1;
+    }
+
+    for(i=0; i<z; i++)
+    {
+        for(j=0;j<a;j++)
+        {
+            if(j==z+i-1||j==z-i-1)
+            {printf("*");}
+            else
+            {printf("_");}
+        }
+        printf("\n");
+    }
+    return 0;
+}
